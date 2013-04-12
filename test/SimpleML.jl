c636e0835a28b6d07296d7d35b2babefb071beb4
Avik Sengupta <avik@sengupta.net>
2013-04-12 21:03:51 +0100

    add tests

---
+++ test/SimpleML.jl
@@ -0,0 +1,26 @@
+using SimpleML
+using Test
+
+using HDF5
+using JLD
+
+
+
+function testLogisticOnevsAll()
+    f=jldopen(joinpath(Pkg.dir(),"SimpleML/test/mnist.jld"), "r")
+    X=read(f, "X")
+    y=read(f, "y")
+    m = size(X, 1); #number of data points
+    n = size(X, 2); #number of features
+    X = [ones(m, 1) X];
+
+    all_theta = logisticAll(X, y, .1, 10)
+
+    @test_approx_eq mean((int(p.==y))) .9646
+
+
+end
+
+
+
+testLogisticOnevsAll()

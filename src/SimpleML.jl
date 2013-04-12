0d1f30bf708f00dcf9e952faf6a737f272e5e667
Avik Sengupta <avik@sengupta.net>
2013-04-12 21:03:25 +0100

    implement multiple labels

--- src/SimpleML.jl
+++ src/SimpleML.jl
@@ -3,26 +3,23 @@ module SimpleML
 using Optim
 
 
-export logisticPredict, logistic, logisticReg
+export logisticPredict, logistic, logisticReg, logisticAll, logisticPredictAll
 
 sigmoid(x) = (1 + exp (-1 * x)) .^ -1
 
-#theta : coefficients
-#X : feature matrix
-#y : training labels
-#grad : output vector for gradient
-function logisticCost(theta, X, y)
-    m = length(y) #size of training set
 
-    h = sigmoid(X * theta)
+# function logisticCost(theta, X, y)
+#     m = length(y) #size of training set
 
-    sum =0
-    for i = 1:m 
-        sum = sum + ( -1 * y[i] * log(h[i]) - (1-y[i]) * log (1-h[i]))
-    end
+#     h = sigmoid(X * theta)
 
-    return sum / m
-end
+#     sum =0
+#     for i = 1:m 
+#         sum = sum + ( -1 * y[i] * log(h[i]) - (1-y[i]) * log (1-h[i]))
+#     end
+
+#     return sum / m
+# end
 
 function logisticCostGradient(theta, X, y)
     grad = zeros(size(theta))
@@ -39,7 +36,34 @@ function logisticCostGradient(theta, X, y)
     return grad
 end
 
-function logisticCostReg(theta, X, y, lambda)
+#theta : coefficients
+#X : feature matrix
+#y : training labels
+#grad : output vector for gradient
+function logisticCost(theta, X, y, grad)
+    m = length(y) #size of training set
+    h = sigmoid(X * theta)
+
+    sum =0
+    for i = 1:m 
+        sum = sum + ( -1 * y[i] * log(h[i]) - (1-y[i]) * log (1-h[i]))
+    end
+
+    if !(grad === nothing)
+        for i = 1:m
+            for j = 1:length(theta)
+                grad[j] =  grad[j]  + (h[i] - y[i])*X[i,j]
+           end
+        end
+
+        map!(x->x/m , grad)
+    end 
+
+    return sum/m
+
+end 
+
+function logisticCostReg(theta, X, y, grad, lambda)
     m = length(y) #size of training set
     n = length(theta) #number of features
 
@@ -53,6 +77,20 @@ function logisticCostReg(theta, X, y, lambda)
         sum = sum + (lambda/2) * (theta[j] ^ 2);
     end
 
+    if !(grad === nothing)
+        for j=1:n
+            for i = 1:m
+                grad[j] =  grad[j]  + (h[i] - y[i])*X[i,j];
+            end
+
+           if j==1
+                grad[j] = grad[j]/m;
+           else 
+                grad[j] = ( grad[j] + lambda* theta[j]) / m;
+           end
+        end
+    end
+
     return sum / m
 end
 
@@ -62,50 +100,48 @@ function logisticCostGradientReg(theta, X, y, lambda)
     n = length(theta) #number of features
 
     h = sigmoid(X * theta)
-    for j=1:n
-        for i = 1:m
-            grad[j] =  grad[j]  + (h[i] - y[i])*X[i,j];
-       end
 
-       if j==1
-            grad[j] = grad[j]/m;
-       else 
-            grad[j] = ( grad[j] + lambda* theta[j]) / m;
-       end
-
-    end
     return grad
 end
 
 function logistic(X, y)
     initial_theta = zeros(size(X,2))
-    grad = zeros(size(initial_theta));
-    initial_cost = logisticCost(initial_theta, X, y)
-    println("Cost Function at intial theta: $initial_cost")
 
-    results = optimize((t)->logisticCost(t, X, y),
-                        (t)->logisticCostGradient(t, X, y),  initial_theta)
+    results, fval, fcount, converged = cgdescent((g,t)->logisticCost(t, X, y, g),  initial_theta)
 
-    println(results)
+    println("Converged: $converged @ $(fval[end]) with coefficients $results in $fcount iterations. ")
 
-    return results.minimum
+    return results
 end
 
 function logisticReg(X, y, lambda)
     initial_theta = zeros(size(X,2))
-    grad = zeros(size(initial_theta))
-    initial_cost = logisticCostReg(initial_theta, X, y, lambda)
-    println("Cost Function at intial theta: $initial_cost")
-    results = optimize((t)->logisticCostReg(t, X, y, lambda),
-                        (t)->logisticCostGradientReg(t, X, y, lambda),  initial_theta)
+    
+    results, fval, fcount, converged = cgdescent((g,t)->logisticCostReg(t, X, y, g, lambda),  initial_theta)
 
-    println(results)
+    println("Converged: $converged @ $(fval[end]) with coefficients $results in $fcount iterations. ")
 
-    return results.minimum
+    return results
+end
+
+function logisticAll(X, y, lambda, num_labels)
+
+    m = size(X, 1); #number of data points
+    n = size(X, 2); #number of features
+
+    all_theta = zeros(num_labels, n)
+
+    for c=1:num_labels
+        initial_theta = zeros(n , 1)
+        all_theta[c,:]=logisticReg(X,int((y.==c)),lambda)[:]
+    end
+
+     return all_theta
+
 end
 
 function logisticPredict(theta, X )
-    m = size(X, 1) #Number of training examples
+    m = size(X, 1) 
     p = zeros(m, 1)
     h = sigmoid(X * theta);
 
@@ -118,6 +154,26 @@ function logisticPredict(theta, X )
 
     end
     return p
+end
+
+function logisticPredictAll(all_theta, X)
+    m = size(X, 1) #Number of training examples
+    n = size(X, 2) #Number of features
+    num_labels = size(all_theta, 1)
+    all_h = zeros(num_labels, m)
+    for c = 1: num_labels
+        theta = all_theta[c,:]
+        h = sigmoid(X * theta')
+        all_h[c,:] = h[:]
+    end    
+
+    ix = zeros(m)
+
+    for i = 1:m
+        _ , ix[i] = findmax(all_h[:,i])
+    end
+
+    return ix
 end
 
 
